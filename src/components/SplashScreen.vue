a590f99c5d2f2e910061275b1b7c4495b11bf3b2
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-11 15:19:37 -0500

    SplashScreen

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="splashascreen">
+    <img src="@/assets/cashflow-icon.svg" alt="Logo" />
+    <h1><span>Cash</span>Flow</h1>
+  </div>
+</template>
+
+<style scoped>
+.splashascreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
