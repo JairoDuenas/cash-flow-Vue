86c721885e150c0fa978b68456261d91c07e8195
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-12 16:15:20 -0500

    ultimando detalles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,7 +12,7 @@
         :amount="amount"
       >
         <template #graphic>
-          <Graphic :amounts="amounts" />
+          <Graphic :amounts="amounts" @select="select" />
         </template>
         <template #action>
           <Action @create="create" />
@@ -60,7 +60,7 @@ export default {
         .map((m) => m.amount);
 
       return lastDays.map((m, i) => {
-        const lastMovements = lastDays.slice(0, i);
+        const lastMovements = lastDays.slice(0, i + 1);
         return lastMovements.reduce((suma, movement) => {
           return suma + movement;
         }, 0);
@@ -94,6 +94,10 @@ export default {
     },
     save() {
       localStorage.setItem("movements", JSON.stringify(this.movements));
+    },
+    select(el) {
+      console.log(el);
+      this.amount = el;
     },
   },
 };
